2169966db9ef5bb91675f8933979e1bf33b9d061
Inepsa <inepsa@gmail.com>
2024-05-05 22:46:27 -0400

    update manifest

--- gilded_convert/fxmanifest.lua
+++ gilded_convert/fxmanifest.lua
@@ -1,7 +1,9 @@
 
 fx_version "cerulean"
 rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
+author "InepsaOG"
 games {"rdr3"}
+description 'A script that converts items and weapons'
 
 shared_scripts {
     'config.lua',
